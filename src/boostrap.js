4d78d111e8b3465d5d5f5b36061307d9b1a83458
williamsduarte <wsduarte@outlook.com>
2018-03-31 15:01:15 -0400

    Add correções de plugins

--- src/boostrap.js
+++ src/boostrap.js
@@ -11,15 +11,12 @@ import './assets/css/main.css'
 import 'bootstrap-sweetalert/dist/sweetalert.css'
 import './assets/css/separate/vendor/sweet-alert-animations.min.css'
 
-import 'popper.js/dist/popper.js'
 import 'tether/dist/js/tether.js'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'lobipanel/dist/js/lobipanel.js'
 import './assets/js/plugins.js'
 import 'bootstrap-sweetalert/dist/sweetalert.min'
-
 import 'jquery-ui-dist/jquery-ui.js'
+import 'jquery-match-height/dist/jquery.matchHeight.js'
 
-
-import './assets/js/lib/match-height/jquery.matchHeight.js'
 import './assets/js/app.js'
